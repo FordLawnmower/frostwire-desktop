17e65dd2e98b01f5b3b47fd0c8f609fe3abf082f
erichpleny <erich.pleny@gmail.com>
2013-01-07 16:27:55 +0100

    FWPlayer - adding file name text to media player in new fw6 UI.

--- gui/com/limegroup/gnutella/gui/fw6ui/mediaplayerpanel/MPProgressPanel.java
+++ gui/com/limegroup/gnutella/gui/fw6ui/mediaplayerpanel/MPProgressPanel.java
@@ -2,7 +2,11 @@ package com.limegroup.gnutella.gui.fw6ui.mediaplayerpanel;
 
 
 import java.awt.BorderLayout;
+import java.awt.Font;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import com.frostwire.gui.mplayer.ProgressSlider;
@@ -10,12 +14,14 @@ import com.frostwire.gui.mplayer.ProgressSliderListener;
 import com.frostwire.gui.player.MPlayerUIEventHandler;
 import com.frostwire.gui.player.MediaPlayer;
 import com.frostwire.gui.player.MediaPlayerAdapter;
+import com.frostwire.gui.player.MediaSource;
 
 public class MPProgressPanel extends JPanel {
 
 	private static final long serialVersionUID = 2626046650466778244L;
 
-	ProgressSlider progressSlider = null;
+	private ProgressSlider progressSlider = null;
+	private JLabel currentMediaText = null;
 	
 	public MPProgressPanel() {
 		
@@ -26,14 +32,27 @@ public class MPProgressPanel extends JPanel {
 				progressSlider.setTotalTime(mediaPlayer.getDurationInSecs());
 				progressSlider.setCurrentTime(currentTimeInSecs);
 			}
+			@Override public void mediaOpened(MediaPlayer mediaPlayer, MediaSource mediaSource) {
+				setMediaText(mediaPlayer);
+			}
 		});
 	}
 	
 	private void initializeUI() {
 		setLayout(new BorderLayout());
 		setOpaque(false);
+		setBorder( BorderFactory.createEmptyBorder(12,0,12,0));
+		
+		// wrapper panel
+		JPanel wrapper = new JPanel();
+		wrapper.setLayout( new BoxLayout(wrapper, BoxLayout.PAGE_AXIS));
+		wrapper.setOpaque(false);
 		
 		// title label
+		currentMediaText = new JLabel();
+        currentMediaText.setFont(new Font("DIALOG", Font.PLAIN, 12));
+        wrapper.add(currentMediaText, BorderLayout.PAGE_START);
+        clearMediaText();
 		
 		// progress slider
 		progressSlider = new ProgressSlider();
@@ -48,7 +67,21 @@ public class MPProgressPanel extends JPanel {
 				MPlayerUIEventHandler.instance().onProgressSlideEnd();
 			}
 		});
-		add(progressSlider, BorderLayout.CENTER);
 		
+		wrapper.add(progressSlider, BorderLayout.CENTER);
+		add(wrapper);
+	}
+	
+	private void setMediaText(MediaPlayer mediaPlayer) {
+		
+		MediaSource source = mediaPlayer.getCurrentMedia();
+		
+		currentMediaText.setText(source.getTitleText());
+		currentMediaText.setToolTipText(source.getToolTipText());
+	}
+	
+	private void clearMediaText() {
+		currentMediaText.setText(" ");
+		currentMediaText.setToolTipText("");
 	}
 }
